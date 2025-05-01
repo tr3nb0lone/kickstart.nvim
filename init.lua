3f8ebf46a359c70a96dc8a69c1793910f24ba220
tr3nb0lone <amanhaileg@gmail.com>
2025-05-01 11:58:11 +0300

    Task:
    Included vim-be-good for the sake of training. Modularization continued.

--- init.lua
+++ init.lua
@@ -369,26 +369,9 @@ require('lazy').setup({
     end,
   },
 
+  -- VimBEgood
+  { 'ThePrimeagen/vim-be-good' },
   -- lazygit
-  {
-    'kdheepak/lazygit.nvim',
-    lazy = false,
-    cmd = {
-      'LazyGit',
-      'LazyGitConfig',
-      'LazyGitCurrentFile',
-      'LazyGitFilter',
-      'LazyGitFilterCurrentFile',
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-      'nvim-telescope/telescope.nvim',
-      'nvim-lua/plenary.nvim',
-    },
-    config = function()
-      require('telescope').load_extension 'lazygit'
-    end,
-  },
 
   { 'MaximilianLloyd/ascii.nvim', requires = {
     'MunifTanjim/nui.nvim',
